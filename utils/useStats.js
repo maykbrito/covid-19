9c5c4f27910343aaddc227bc818dd021e24183ec
Mayk Brito <maykbrito@gmail.com>
2020-03-19 19:52:56 -0300

    chore: update useStats / using Stats with Selector

--- utils/useStats.js
+++ utils/useStats.js
@@ -2,17 +2,28 @@ import { useState, useEffect } from "react"
 
 export default function useStats(url) {
     const [ stats, setStats ] = useState()
+    const [ loading, setLoading ] = useState(false)
+    const [ error, setError ] = useState()
 
     useEffect(() => {
+        setLoading(true);
+
         async function fetchData() {
             let data = await fetch(url)
             .then(res => res.json())
+            .catch(err => setError(err))
             
+
             setStats(data)
+            setLoading(false);
         }
 
         fetchData()
-    }, [])
+    }, [url])
 
-    return stats
+    return {
+        stats,
+        loading,
+        error
+    }
 }
