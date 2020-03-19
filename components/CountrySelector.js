e7965be74097a44b6ac7e3841cd14854877a3a09
maykos_launchbase <mayk@rocketseat.com.br>
2020-03-19 19:07:38 -0300

    feat: add onchange select

--- components/CountrySelector.js
+++ components/CountrySelector.js
@@ -1,21 +1,27 @@
+import { useState } from "react"
+
 import useStats from "../utils/useStats"
 
 export default function CountrySelector() {
     const countries = useStats("https://covid19.mathdro.id/api/countries")
+    const [ selectedCountry, setSelectedCountry ] = useState("BR");
 
     if (!countries) return <p>Loading...</p>
 
     console.log(countries)
 
+
     const options = Object.entries(countries.countries).map(([country, code], key) => (
-        <option key={key} value={code}>
+        <option key={key} value={countries.iso3[code]}>
             {country}
         </option>
     ))
 
     return (
         <section className="country-selector">
-            <select>
+            <select onChange={e => {
+              setSelectedCountry(e.target.value)
+            }}>
                 {options}
             </select>
         </section>
