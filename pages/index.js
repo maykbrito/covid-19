3f76f076d1c33a68e85884b46a6a3e41d511e7b2
maykos_launchbase <mayk@rocketseat.com.br>
2020-03-19 12:13:21 -0300

    feat: consume api

--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,26 @@
+import { useState, useEffect } from "react"
+
+function useStats() {
+    const [ stats, setStats ] = useState()
+
+    useEffect(() => {
+        async function fetchData() {
+            let data = await fetch("https://covid19.mathdro.id/api/countries/BR")
+            .then(res => res.json())
+            
+            setStats(data)
+        }
+
+        fetchData()
+    }, [])
+
+    return stats
+}
+
 export default function IndexPage() {
+    const stats = useStats()
+
+    if (!stats) return <p>Loading...</p>
+
     return <h1>Great!</h1>
 }
