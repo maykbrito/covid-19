5d8fdae7bd08041a848ebe492c2a08f13593e8f9
Mayk Brito <maykbrito@gmail.com>
2020-03-21 19:07:24 -0400

    feat: add images

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,8 @@
 import CountrySelector from  "../components/CountrySelector"
 import { createGlobalStyle } from 'styled-components'
+import styled from 'styled-components'
+
+import logo from '../images/virus.png'
 
 const GlobalStyle = createGlobalStyle`
   * {
@@ -24,13 +27,30 @@ const GlobalStyle = createGlobalStyle`
     width: 90%;
     max-width: 800px;
 
-    margin: 15vh auto;
+    margin: 32px auto 0;
   }
 `
 
+const Logo = styled.div`
+  margin: 32px auto 0;
+  width: 200px;
 
+  display: flex;
+  place-items: center;
+
+  font-size: 24px;
+
+  img {
+    width: 60px;
+    margin-right: 8px;
+  }
+`
 export default function IndexPage() {
     return <>
+        <Logo>
+          <img src={logo} alt="Covid-19" />
+          COVID-19
+        </Logo>
         <main>
             <CountrySelector></CountrySelector>
         </main>
